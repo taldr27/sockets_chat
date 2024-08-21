18f2997a9369e91d9c0fb2b5d7e5136d856fea1d
taldr27 <diegogarciaq98@gmail.com>
2024-08-21 13:02:40 -0500

    Add basic layout test for chat

--- express_sockets/src/index.ts
+++ express_sockets/src/index.ts
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "http://127.0.0.1:5500",
   },
 });
 
@@ -16,7 +16,9 @@ app.get("/", (_req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  socket.on("chat", (message) => {
+    io.emit("chat", message);
+  });
 });
 
 server.listen(port, () => {
